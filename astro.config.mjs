711a0cecac9c122de00cbe833d73ebfe39e9a7bc
Hyper-Spectrum <hyperspectrumteam@gmail.com>
2025-05-10 16:21:40 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://Hyper-Spectrum.github.com/",
+	site: "https://Hyper-Spectrum.github.com/Hyper-Spectrum",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
